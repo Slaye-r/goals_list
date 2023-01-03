6a17be91c650b507d57b76cf7694bd3c343dc31b
Slaye-r <111708118+Slaye-r@users.noreply.github.com>
2023-01-03 15:16:16 +0530

    updated

--- src/Components/Goals/Input/Goals.js
+++ src/Components/Goals/Input/Goals.js
@@ -1,7 +1,34 @@
-import "./Goals.css";
+import styles from "./Goals.module.css";
 import { useState } from "react";
-
 import Button from "../../UI/Button";
+//import styled from "styled-components";
+
+/*const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red' :'black' )};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' :'#ccc' )};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+    background: ${props => (props.invalid ? 'rgb(248, 210, 210);' :'#ccc' )}
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;*/
 
 const Goals = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -24,7 +51,9 @@ const Goals = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
